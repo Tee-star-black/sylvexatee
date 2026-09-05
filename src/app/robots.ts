dcb2414080098587dca7d1d3ae2505583fd18b8f
Tee-star-black <tntshabeleng23@gmail.com>
2026-09-05 20:06:14 +0200

    seo: allow crawlers to render Next assets

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -21,7 +21,7 @@ export default function robots(): MetadataRoute.Robots {
       {
         userAgent: "*",
         allow: "/",
-        disallow: ["/api/", "/_next/"],
+        disallow: ["/api/"],
       },
     ],
 
